a617fab86e7a2f5718be2de2ac6a489cfecc8549
Afshawn <me@afshawn.org>
2023-03-17 16:55:21 +0000

    updated README.md

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
    name='ezmesh',
-   version='1.0',
+   version='2.0',
    description='the open source parametric CFD mesh generator',
    author='Afshawn Lotfi',
    author_email='',
