2ee660224b44cda8479324d5b556551c3cd56b59
Afshawn Lotfi <me@afshawn.org>
2023-04-02 07:21:40 +0000

    added target points

--- ezmesh/mesh.py
+++ ezmesh/mesh.py
@@ -1,9 +1,10 @@
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from enum import Enum
 from typing import Dict, List, Optional
 import numpy.typing as npt
 import numpy as np
+
 
 class ElementType(Enum):
     LINE = 1
@@ -15,6 +16,7 @@ class ElementType(Enum):
     PYRAMID = 7
     POINT = 15
 
+
 @dataclass
 class Mesh:
     dim: int
@@ -22,3 +24,26 @@ class Mesh:
     element_types: List[ElementType]
     points: npt.NDArray[np.float64]
     markers: Dict[str, List[npt.NDArray[np.uint16]]]
+    target_points: Dict[str, np.uint16] = field(default_factory=dict)
+
+    def get_marker_length(self, marker_name: str):
+        marker_length = 0
+        for elements in self.markers[marker_name]:
+            assert len(elements) == 2
+            marker_length +=  np.linalg.norm(self.points[elements[0]] - self.points[elements[1]])
+        return marker_length
+
+    def add_target_point(self, target_name: str, marker_name: str, proportion: float):
+        if marker_name not in self.markers:
+            raise ValueError(f"Marker '{marker_name}' not found in mesh")
+        if proportion < 0 or proportion > 1:
+            raise ValueError(f"Proportion {proportion} must be in the range [0, 1]")
+        total_marker_length = self.get_marker_length(marker_name)
+        current_marker_length = 0
+        for elements in self.markers[marker_name]:
+            assert len(elements) == 2
+            current_marker_length +=  np.linalg.norm(self.points[elements[0]] - self.points[elements[1]])
+            if current_marker_length / total_marker_length >= proportion:
+                self.target_points[target_name] = elements[0]
+                return
+        raise ValueError(f"Proportion {proportion} is too large for marker '{marker_name}'")
--- ezmesh/utils/geometry.py
+++ ezmesh/utils/geometry.py
@@ -10,19 +10,19 @@ def get_group_name(selector: str) -> str:
     return selector
 
 
-def get_property(property: Optional[PropertyType[T]], index: int, selector: Optional[str] = None, default: T = None) -> T:
+def get_property(property: Optional[PropertyType[T]], index: int, marker: Optional[str] = None, default: T = None) -> T:
     if property is None:
         return default
     elif isinstance(property, list):
         return property[index]
     elif isinstance(property, dict):
-        if selector is None:
+        if marker is None:
             return default
-        label_wildcard = f"{get_group_name(selector)}/*"
-        if selector in property:
-            return property[selector]
-        elif label_wildcard in property:
-            return property[label_wildcard]
+        marker_wildcard = f"{get_group_name(marker)}/*"
+        if marker in property:
+            return property[marker]
+        elif marker_wildcard in property:
+            return property[marker_wildcard]
         return default
     else:
         return property
