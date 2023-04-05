ddb5afc08a675d843df5057597713c373c7d864b
Afshawn Lotfi <me@afshawn.org>
2023-04-05 02:36:07 +0000

    *added custom su2 export that accounts for gmsh unused points *added proper point sizing and mesh bounding box

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
    name='ezmesh',
-   version='2.0',
+   version='3.0',
    description='the open source parametric CFD mesh generator',
    author='Afshawn Lotfi',
    author_email='',
