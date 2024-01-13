12e2aa06ca23deb9efc1c5a71d374690a62ecb25
Afshawn Lotfi <me@afshawn.org>
2024-01-12 20:48:55 -0500

    Updated ipywidgets version to latest and README.md

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
    name='ezmesh',
-   version='3.6.0',
+   version='3.6.5',
    description='the open source parametric CFD mesh generator',
    author='Afshawn Lotfi',
    author_email='',
@@ -12,9 +12,9 @@ setup(
    install_requires=[
     "numpy",
     "gmsh",
-    "ipywidgets==7.6",
+    "ipywidgets",
     "pythreejs",
-    "su2fmt @ git+https://github.com/Turbodesigner/su2fmt.git",
+    "su2fmt @ git+https://github.com/OpenOrion/su2fmt.git",
     "shapely",
     "scipy"
    ]
